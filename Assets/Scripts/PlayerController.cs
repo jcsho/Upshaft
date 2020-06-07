2991a1d91ff85899a60700bbd84cd98222cfb28d
Justin Ho <justinhodev@gmail.com>
2020-06-07 11:01:32 -0700

    Implement Basic Movement
    - add horizontal movement for player
    - add temporary auto-bounce on platforms

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D _rigidbody2d;
+    private float _moveInput;
+    private float _speed = 10f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _moveInput = Input.GetAxis("Horizontal");
+        _rigidbody2d.velocity = new Vector2(_moveInput * _speed, _rigidbody2d.velocity.y);
+    }
+}
