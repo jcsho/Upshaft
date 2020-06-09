608de82e7ed14631d0cd53cd53d56aebe3d7a52f
Justin Ho <justinhodev@gmail.com>
2020-06-08 18:28:21 -0700

    Start Coin Mechanic
    - add coin script which disappears on collision with player
    - TODO bind to platform
    - TODO add points system

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        Debug.Log("Collided with " + other.name);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // TODO: add a point
+            Destroy(this.gameObject);
+        }
+    }
+}
